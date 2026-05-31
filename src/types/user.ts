963f7ce35c56d91bd9bfd2fd6c1bb62eafc0bf69
gehleegian <maringian779@gmail.com>
2026-05-31 23:36:37 +0800

    Update user.ts

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,10 +1,15 @@
 export type UserRole = 'Admin' | 'User';
+export type UserAccountStatus = 'active' | 'inactive';
 
 export interface UserData {
   id?: string;
   name: string;
+  fullName?: string;
   email: string;
+  contactNumber?: string;
   role: UserRole;
   createdAt?: string;
+  updatedAt?: string;
+  accountStatus?: UserAccountStatus;
   status?: 'online' | 'offline';
 }
